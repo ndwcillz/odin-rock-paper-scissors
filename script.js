7808322bbd4984419d4bd059e8bcfda325763df1
William Zhou <ndxcxnter@gmail.com>
2026-06-05 21:26:25 -0500

    Create playRound Function to Simualate One Round
    Wrote logic to play a single round and return
    a message through console.log for the result of
    the game and also updated human/computer score

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    let humanChoiceLowerCase = humanChoice.toLowerCase();
+
+    if (humanChoiceLowerCase == computerChoice) {
+        console.log("You tied! Both of you selected " + humanChoice + ".");
+    } else if (humanChoiceLowerCase == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+    } else if (humanChoiceLowerCase == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+        } else {
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+        }
+    } else {
+        if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore++;
+        } else {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
