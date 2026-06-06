7ecf9b3f0ca54001a965c30684ed3d9bd4bf1ee4
William Zhou <ndxcxnter@gmail.com>
2026-06-05 20:57:45 -0500

    Create Logic to Get Computer Choice
    Wrote the function to get the computer choice
    to randomly return either "rock" "paper" or
    "scissors" and tested with console.log

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3) + 1;
+
+    if (choice == 1) {
+        return "rock";
+    } else if (choice == 2) {
+        return "scissors";
+    } else {
+        return "paper";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
